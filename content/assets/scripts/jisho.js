c54433c84f2b290d525ec7d8933d992d4aea44d0
NSDawn <tnahsin.airus@gmail.com>
2023-09-11 18:37:17 -0700

    remove nocors

--- content/assets/scripts/jisho.js
+++ content/assets/scripts/jisho.js
@@ -3,11 +3,12 @@ let test;
 lookup("potato", test)
 
 function lookup(search_query) {
-    fetch("https://jisho.org/api/v1/search/words?keyword=" + search_query, {
-        mode: "no-cors",
-    })
+    fetch("https://jisho.org/api/v1/search/words?keyword=" + search_query)
         .then(response => {
             if (!response.ok) {
+                console.log(response.status);
+                console.log(response.statusText);
+
                 throw new Error("Network response was not ok")
             }
             return response.json();
