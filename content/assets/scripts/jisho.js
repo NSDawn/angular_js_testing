49d1f6f5ebd5ca286c07146f1b9e47f305b3ad8b
NSDawn <tnahsin.airus@gmail.com>
2023-09-12 16:44:34 -0700

    jisho is working

--- content/assets/scripts/jisho.js
+++ content/assets/scripts/jisho.js
@@ -1,21 +1,50 @@
+const RESPONSEDIV = document.getElementsByClassName("search_response")[0]; 
+const QUERYDIV = document.getElementsByClassName("search_query")[0];
+let query_last_value = "";
 
-let test;
-lookup("potato", test)
 
-function lookup(search_query) {
-    fetch("https://jisho.org/api/v1/search/words?keyword=" + search_query)
-        .then(response => {
-            if (!response.ok) {
-                console.log(response.status);
-                console.log(response.statusText);
+angular.module("jishoApp", []) 
+    .controller("jishoController", ["$scope", "$q", function jishoController($scope, $q) {
+        $scope.search_query = "";
+        //$scope.data = {};
+        //$scope.data = JSON.parse($scope._data);
+        
+        function lookup(search_query) {
+            return fetch("https://jotoba.de/api/search/words", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    "query": search_query,
+                    "language": "English",
+                    "no_english": true,
+                }),
+            }).then ((res) => {
+                if (!res.ok) {
+                    console.error('Network response was not ok');
+                    return {};
+                }
+                return res.json();
+            });
+        }
+        
+        $scope.update = function () {
+            $scope.msg = "fetching...";
+            $scope.data = {}; 
 
-                throw new Error("Network response was not ok")
-            }
-            return response.json();
-        }).then(data => {
-            console.log(data);
-        }).catch(error => {
-            console.error("Fetch error:", error )
-        })
-}
+            lookup($scope.search_query).then((result) => {
+                $scope.$apply( () => { 
+                    $scope.data = result;
+                    $scope.msg = "";
+
+                    if (result.words == null) {$scope.msg = "no entries found."}
+                });
+            }).catch(function(error) {
+                console.error('Error:', error);
+                $scope.data = 'Error fetching data';
+            });
+        }
+    }]);
+
 
